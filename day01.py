7e6e373cca1867a06883d25fd113f238dd54b9e2
Leevihovatov <leevi.hovatov@gmail.com>
2023-12-01 11:37:30 +0200

    Day 1 silver and gold at the same time

--- day01.py
+++ day01.py
@@ -2,28 +2,11 @@ def main():
     with open("input01.txt", "r") as f:
         data = f.read().splitlines()
     
-    answer = 0
+    silver = 0
+    gold = 0
     
     numbers = ['one','two','three','four','five','six','seven','eight','nine']
-    for line in data:
-        
-        # Creating a new line with only numbers by concatenating
-        # numbers to new one as they come up in the data
-        # Skipped in part 1 
-        fixed_line = ""
-        for i in range(len(line)):
-            if line[i].isnumeric():
-                fixed_line += line[i]
-            else:
-                # Checking if the line start with any written number at the current index
-                for n in range(len(numbers)):
-                    try:
-                        if (line[i:].startswith(numbers[n])):
-                            fixed_line += str(n+1)
-                            break
-                    except:
-                        pass
-        line = fixed_line
+    for line in data:        
 
         # Finding the first digit
         for i in range(len(line)):
@@ -38,8 +21,29 @@ def main():
                 break
         
         number = int(first+last)
-        answer += number    
-    print(answer)
+        silver += number
+        
+        ## Gold star
+        # Creating a new line with only numbers by concatenating
+        # numbers to new line as they come up in the original line 
+        fixed_line = ""
+        for i in range(len(line)):
+            if line[i].isnumeric():
+                fixed_line += line[i]
+            else:
+                # Checking if the line start with any written number at the current index
+                for n in range(len(numbers)):
+                    try:
+                        if (line[i:].startswith(numbers[n])):
+                            fixed_line += str(n+1)
+                            break
+                    except:
+                        pass
+        number = int(fixed_line[0]+fixed_line[-1])
+        gold += number
+    
+    print(f'Silver: {silver}')
+    print(f'Gold: {gold}')
 
 if __name__ == "__main__":
     main()
--- template.py
+++ template.py
@@ -1,7 +1,14 @@
 def main():
     with open("test.txt", "r") as f:
-        data = f.readlines()
+        data = f.read().splitlines()
+    
+    silver = 0
+    gold = 0
 
+
+
+    print(f'Silver: {silver}')
+    print(f'Gold: {gold}')
 
 if __name__ == "__main__":
     main()
